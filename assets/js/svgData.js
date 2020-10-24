dc6431e93314e95a42b919ae33656e529134d15f
unknown <run128@naver.com>
2020-10-24 14:20:11 +0900

    image 링크 수정

--- assets/js/svgData.js
+++ assets/js/svgData.js
@@ -1,41 +1,41 @@
 const svgData = [{
     name: "고양이",
-    url: "assets/images/cat.svg",
+    url: "./assets/images/cat.svg",
 },
 {
     name: "나비",
-    url: "assets/images/butterfly.svg",
+    url: "./assets/images/butterfly.svg",
 },
 {
     name: "뱀",
-    url: "assets/images/snake.svg",
+    url: "./assets/images/snake.svg",
 },
 {
     name: "사자",
-    url: "assets/images/lion.svg",
+    url: "./assets/images/lion.svg",
 },
 {
     name: "소",
-    url: "assets/images/bull.svg",
+    url: "./assets/images/bull.svg",
 },
 {
     name: "암모나이트",
-    url: "assets/images/ammonite.svg",
+    url: "./assets/images/ammonite.svg",
 },
 
 {
     name: "이집트 그림",
-    url: "assets/images/egypt_mural.svg",
+    url: "./assets/images/egypt_mural.svg",
 },
 {
     name: "장미",
-    url: "assets/images/rose.svg",
+    url: "./assets/images/rose.svg",
 },
 {
     name: "전갈",
-    url: "assets/images/scorpion.svg",
+    url: "./assets/images/scorpion.svg",
 },
 {
     name: "종이비행기",
-    url: "assets/images/paper_airplane.svg",
+    url: "./assets/images/paper_airplane.svg",
 }]
